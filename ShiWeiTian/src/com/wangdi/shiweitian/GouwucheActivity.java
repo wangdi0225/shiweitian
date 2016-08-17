5315a0b5cf590ef184e3d417f0865c98d0144722
ListHu <1126369195@QQ.COM>
2016-08-17 18:49:59 +0800

    no message

--- ShiWeiTian/src/com/wangdi/shiweitian/GouwucheActivity.java
+++ ShiWeiTian/src/com/wangdi/shiweitian/GouwucheActivity.java
@@ -5,9 +5,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
@@ -19,7 +19,7 @@ import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class GouwucheActivity extends FragmentActivity {
+public class GouwucheActivity extends Activity {
 	private ListView listview;
 	private TextView textView;
 	private ImageView imageView;
